4d6e525a5a6b8500d6b6bf7e8204b57819bf9e91
brellin <cekakid@gmail.com>
2019-12-03 01:05:26 -0600

    fix modules

--- src/03_modules.py
+++ src/03_modules.py
@@ -12,9 +12,7 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
-import fileinput
-
-for line in fileinput.input(sys.argv[0]):
+for line in sys.argv:
     print(line)
 
 # Print out the OS platform you're using:
